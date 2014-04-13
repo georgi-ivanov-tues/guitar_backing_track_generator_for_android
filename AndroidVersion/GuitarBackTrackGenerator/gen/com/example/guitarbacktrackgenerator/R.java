2c50b95e22887c4b135161ca448d8910a87f6de5
nedelcho-delchev-tues <nedelcho_delchev@abv.bg>
2014-04-13 22:29:49 +0300

    Changing the UI

--- AndroidVersion/GuitarBackTrackGenerator/gen/com/example/guitarbacktrackgenerator/R.java
+++ AndroidVersion/GuitarBackTrackGenerator/gen/com/example/guitarbacktrackgenerator/R.java
@@ -24,15 +24,16 @@ public final class R {
         public static final int background=0x7f020000;
         public static final int background2=0x7f020001;
         public static final int background_old=0x7f020002;
-        public static final int guitar1=0x7f020003;
-        public static final int guitar2=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int icon=0x7f020006;
+        public static final int button=0x7f020003;
+        public static final int guitar1=0x7f020004;
+        public static final int guitar2=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int icon=0x7f020007;
     }
     public static final class id {
         public static final int B=0x7f09002b;
         public static final int Choice=0x7f09001d;
-        public static final int Key=0x7f090010;
+        public static final int Key=0x7f09000c;
         public static final int LinearLayout=0x7f090007;
         public static final int Title=0x7f090018;
         public static final int action_settings=0x7f090028;
@@ -40,23 +41,23 @@ public final class R {
         public static final int buttonAddToFavourites=0x7f090023;
         public static final int buttonAddToRecordings=0x7f090026;
         public static final int buttonAsharp=0x7f09002a;
-        public static final int buttonCalm=0x7f09000e;
+        public static final int buttonCalm=0x7f090011;
         public static final int buttonClear=0x7f090016;
         public static final int buttonCredits=0x7f09001c;
         public static final int buttonDoesTrackExist=0x7f090020;
         public static final int buttonExit=0x7f090002;
         public static final int buttonFavourites=0x7f09001a;
         public static final int buttonGenerate=0x7f090019;
-        public static final int buttonHeavy=0x7f09000d;
-        public static final int buttonMaj=0x7f09000f;
-        public static final int buttonMin=0x7f090013;
-        public static final int buttonNext=0x7f090012;
+        public static final int buttonHeavy=0x7f09000f;
+        public static final int buttonMaj=0x7f09000e;
+        public static final int buttonMin=0x7f090010;
+        public static final int buttonNext=0x7f090015;
         public static final int buttonOptions=0x7f09001b;
         public static final int buttonPause=0x7f09001f;
         public static final int buttonPlay=0x7f090008;
         public static final int buttonPlayRec=0x7f090025;
-        public static final int buttonPrev=0x7f090011;
-        public static final int buttonRandomize=0x7f090015;
+        public static final int buttonPrev=0x7f09000d;
+        public static final int buttonRandomize=0x7f090017;
         public static final int buttonRemoveAllTracks=0x7f090006;
         public static final int buttonRemoveTrack=0x7f090005;
         public static final int buttonSortBy=0x7f09000a;
@@ -68,10 +69,10 @@ public final class R {
         public static final int textCreatedBy=0x7f090001;
         public static final int textCreditsMenu=0x7f090000;
         public static final int textKey=0x7f09000b;
-        public static final int textMode=0x7f090014;
-        public static final int textStyle=0x7f09000c;
+        public static final int textMode=0x7f090012;
+        public static final int textStyle=0x7f090013;
         public static final int textViewFavouritesMenu=0x7f090003;
-        public static final int textWarning=0x7f090017;
+        public static final int textWarning=0x7f090014;
         public static final int trackName=0x7f090022;
         public static final int youtube_view=0x7f090027;
     }
@@ -115,7 +116,7 @@ public final class R {
         public static final int Pause=0x7f060016;
         public static final int Play=0x7f06000c;
         public static final int Prev=0x7f06000e;
-        public static final int Randomize=0x7f060017;
+        public static final int Random=0x7f060017;
         public static final int SortBy=0x7f06001f;
         public static final int Speed=0x7f06000d;
         public static final int Start=0x7f06000a;
